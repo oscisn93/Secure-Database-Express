e86a990dca64b16109f4bf7d6830cbc891b4fcd9
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-05-21 04:54:05 -0700

    added necessary fields to sessions, async await for working with mysql2/promise lib

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-import fs from 'node:fs/promises';
-import express from 'express';
-import routes from './routes/index.js'
+import express from "express";
+import routes from "./routes/index.js";
 // Constants
 const port = process.env.PORT || 5173;
-const base = process.env.BASE || '/';
-
-// Create http server
+const base = process.env.BASE || "/";
 const app = express();
-
-const compression = (await import('compression')).default;
-const sirv = (await import('sirv')).default;
-
-
+const compression = (await import("compression")).default;
+const sirv = (await import("sirv")).default;
+// Serve static files
 app.use(compression());
-app.use(base, sirv('./static'));
+app.use(base, sirv("./static"));
 // set the view engine to ejs
 app.set("view engine", "ejs");
 app.use(routes);
-
 // Start http server
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`)
+  console.log(`Server started at http://localhost:${port}`);
 });
-
